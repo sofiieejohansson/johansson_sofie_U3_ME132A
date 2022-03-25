14324d065b5c4a968dc348d7bc7358e392ccf252
Sofie Johansson <sofie.johansson.sovde@gmail.com>
2022-03-25 11:37:13 +0100

    trying to use same code as students

--- courses.js
+++ courses.js
@@ -2,16 +2,24 @@
 
 let courses = DATABASE.courses;
 
-function showCourse() {
+function showCourse(id) {
     let div = document.createElement("div");
-    let course = DATABASE.courses
-    div.classList = "container"
+    let course = DATABASE.courses[id];
+    div.classList = "container";
     div.innerHTML = `
-    <header></header>
+    <header>${courses.title} (${courses.totalCredits})</header>
     <div>
-        <div id="">
-            <h4></h4>
-            <div id="">
+        <div id="course">
+            <h4>Course Responsible:</h4>
+            <div id="teachers">
+                ${courses.courseRespinsible}
+            </div>
+            <h4>Teachers:</h4>
+            <div id="teachers">
+                ${courses.teachers}
+            </div>
+            <h4>Students:</h4>
+            <div>
             
             </div>
         </div>
@@ -19,3 +27,14 @@ function showCourse() {
 
     return div;
 }
+
+function showCourses(courses) {
+    let coursesElement = document.getElementById("courses");
+    for (let course of courses) {
+        let courseElement = showcourse(course.courseID)
+        coursesElement.appendChild(courseElement)
+    }
+}
+
+
+
