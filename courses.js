73b75811d4a3d177d6d4c26acd874cb1f5c3c5de
Sofie Johansson <sofie.johansson.sovde@gmail.com>
2022-03-25 14:22:07 +0100

    updates on errors

--- courses.js
+++ courses.js
@@ -1,11 +1,20 @@
 "use strict"
 
-
 let courses = DATABASE.courses;
+
+let allTeachers = DATABASE.teachers;
+
+function courseResponsible (id) {
+    let course = DATABASE.courses[id]
+    let teachersNames = allTeachers.map((teacher) => teacher.firstName + " " + teacher.lastName + " " + `(${teacher.post})`);
+    let res = course.courseResponsible;
+    return teachersNames[res];
+}
 
 function showCourse(id) {
     let div = document.createElement("div");
     let courses = DATABASE.courses[id];
+    let teachers = DATABASE.teachers;
     div.classList = "results";
     div.innerHTML =
     `<header>${courses.title} (total credits: ${courses.totalCredits})</header>
@@ -13,7 +22,7 @@ function showCourse(id) {
         <div id="studentcourses">
         <h3>Course Responsible:</h3>
         <div id="teachers">
-            ${courses.courseResponsible}
+            ${teachers.teachersNames}
         </div>
 
         <h3>Teachers:</h3>
@@ -22,6 +31,7 @@ function showCourse(id) {
         </div>
         <h3>Students:</h3>
         <div>
+        
         </div>
     </div>
     </div>
@@ -37,35 +47,6 @@ function showCourses (courses) {
     }
 }
 
-function render(student) {
-    let studentData = DATABASE.students;
-    let students = [];
-    for (let i = 0; i < student.courses.length; i++) {
-        let id = student.courses[i].courseId;
-        students.push(studentData[id])
-    }
-
-    let studentBox = []
-    for (let i = 0; i < student.length; i++) {
-        let div = document.createElement("div")
-
-        if (student.courses[i].passedCredits == studentData[courses[i].courseId].totalCredits) {
-            let text = div.innerHTML = 
-            `<div class="done"><h2>${students[i].firstName} ${students[i].lastName} (${students[i].courseId.passedCredits})</h2>
-            <p>${student.courses[i].started.semester} ${student.courses[i].started.year}</p>`
-            studentBox.push(text);
-
-        } else {
-            let text = div.innerHTML = 
-            `<div class="notdone"><h2>${students[i].firstName} ${students[i].lastName} (${students[i].courseId.passedCredits})</h2>
-            <p>${student.courses[i].started.semester} ${student.courses[i].started.year}</p>`
-            studentBox.push(text);
-        }
-    }
-    return studentBox.toString().split(",").join("");
-}
-
-
 function searchCourse() {
     return input.value.toLowerCase();
 }
@@ -79,8 +60,8 @@ function courseSearch() {
         document.getElementById("courses").innerHTML = ""
         if ("" == searchCourse()) {
             document.getElementById("courses").innerHTML = ""
-        } else if (courses[i].courses.toLowerCase().includes(searchCourse())) {
-            coursesArray.push(students[i]);
+        } else if (courses[i].title.toLowerCase().includes(searchCourse())) {
+            coursesArray.push(courses[i]);
         }
     }
 
