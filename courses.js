55692c36cd15956d71de24a0cc4878157ecdc0f5
Sofie Johansson <sofie.johansson.sovde@gmail.com>
2022-03-25 15:41:04 +0100

    på rätt väg

--- courses.js
+++ courses.js
@@ -5,20 +5,19 @@ let courses = DATABASE.courses;
 function showCourse(id) {
     let div = document.createElement("div");
     let courses = DATABASE.courses[id];
-    let teachers = DATABASE.teachers;
     div.classList = "results";
     div.innerHTML =
     `<header>${courses.title} (total credits: ${courses.totalCredits})</header>
     <div>
         <div id="studentcourses">
         <h3>Course Responsible:</h3>
-        <div id="teachers">
-            ${teachers.teachersNames}
+        <div id="resTeacher">
+            ${findResponsible(courses)}
         </div>
 
         <h3>Teachers:</h3>
         <div id="teachers">
-            ${courses.teachers}
+            ${findTeachers(courses)}
         </div>
         <h3>Students:</h3>
         <div>
@@ -37,6 +36,34 @@ function showCourses (courses) {
         coursesElement.appendChild(courseElement);
     }
 }
+
+function findResponsible (courses) {
+    let teacherBox = []
+        for (let i = 0; i < DATABASE.teachers.length; i++) {
+        let div = document.createElement("div")
+        if (DATABASE.teachers[i].teacherId == courses.courseResponsible) {
+            let text = div.innerHTML = `
+            <h2>${DATABASE.teachers[i].firstName} ${DATABASE.teachers[i].lastName} (${DATABASE.teachers[i].post})</h2>`
+            teacherBox.push(text);
+    
+        } 
+    }
+    return teacherBox.toString().split(",").join(""); 
+}
+
+function findTeachers (courses) {
+    let otherTeachers = [];
+    for (let i = 0; i < DATABASE.teachers.length; i++) {
+        let div = document.createElement("div");
+        if (DATABASE.teachers[i].teacherId == courses.teachers) {
+            let text = div.innerHTML = `
+            <h2>${DATABASE.teachers[i].firstName} ${DATABASE.teachers[i].lastName} (${DATABASE.teachers[i].post})</h2>`
+            otherTeachers.push(text);
+        }
+    }
+    return otherTeachers.toString().split(",").join("");
+}
+
 
 function searchCourse() {
     return input.value.toLowerCase();
